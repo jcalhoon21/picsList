1f65c7dae2583670e09a305fc0403ab1287dd8fc
jcalhoon21 <jcalhoon21@gmail.com>
2020-04-20 18:48:41 -0700

    Final Commit

--- src/components/ImageList/index.jsx
+++ src/components/ImageList/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './ImageList.css';
+import ImageCard from '../ImageCard';
 
 const ImageList = (props) => {
   console.log(props.images);
   
-  // const images = props.images.map((image) => {
-  //   return <img key={image.id} src={image.urls.regular} alt={image.description} />
-  // });
- // old way above, refactor below
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img key={id} src={urls.regular} alt={description} />
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />
   });
+ // refactor below
+  // const images = props.images.map(({ description, id, urls }) => {
+  //   return <img key={id} src={urls.regular} alt={description} />
+  // });
 
   return <div className="image-list">{images}</div>;
 };
